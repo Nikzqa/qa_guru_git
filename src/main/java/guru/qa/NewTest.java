61869b53abca61a9717756b82de681753c0907f8
Nikz <smoke2989@gmail.com>
2023-10-03 08:54:11 +0300

    new test added

---
+++ src/main/java/guru/qa/NewTest.java
@@ -0,0 +1,11 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NewTest {
+    @Test
+    void NewTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
